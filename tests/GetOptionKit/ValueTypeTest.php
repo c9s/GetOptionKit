80c48eeb0ec135ec3491ebfd56143113ada252eb
Francois Lajoie <snake386@hotmail.com>
2015-06-01 20:46:14 -0400

    updated test for urltype

--- tests/GetOptionKit/ValueTypeTest.php
+++ tests/GetOptionKit/ValueTypeTest.php
@@ -3,6 +3,7 @@ use GetOptionKit\ValueType\BooleanType;
 use GetOptionKit\ValueType\StringType;
 use GetOptionKit\ValueType\FileType;
 use GetOptionKit\ValueType\NumberType;
+use GetOptionKit\ValueType\UrlType;
 
 class ValueTypeTest extends PHPUnit_Framework_TestCase
 {
@@ -13,6 +14,7 @@ class ValueTypeTest extends PHPUnit_Framework_TestCase
         ok( new StringType );
         ok( new FileType );
         ok( new NumberType );
+        ok( new UrlType );
     }
 
 
@@ -23,6 +25,14 @@ class ValueTypeTest extends PHPUnit_Framework_TestCase
         ok( $bool->test('false') );
         ok( $bool->test('0') );
         ok( $bool->test('1') );
+    }
+
+    public function testUrlType()
+    {
+        $url = new UrlType;
+        ok( $url->test('http://t') );
+        ok( $url->test('http://t.c') );
+        $this->assertFalse($url->test('t.c'));
     }
 
 }
